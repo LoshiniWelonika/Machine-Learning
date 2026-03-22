f76d0ff808bb9e65eb20bcb54d82c1e8a6d5203a
LoshiniWelonika <welonikakedapanawala@gmail.com>
2026-03-22 21:21:04 +0530

    Creating an array

---
+++ Numpy/L2-Create_Array.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x = np.array([2, 4, 56, 7, 98])
+print(x)
+print(x.ndim)
+print("\n")
+
+twodArray = np.array([[1,2,3,4], [5,6,7,8]])
+print(twodArray)
+print(twodArray.ndim)
+print("\n")
+
+threedArray = np.array([[[1,2],[3,4]], [[5,6],[7,8]]])
+print(threedArray)
+print(threedArray.ndim)
+print("\n")
+
+fourdArray = np.array([1,2,3,4], ndmin=4)
+print(fourdArray)
+print(fourdArray.ndim)
+print("\n")
